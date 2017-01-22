c3285a5af4f8d4eff74c1039e2f1b3637337cd9f
Justin O'Boyle <justin@justinoboyle.com>
2017-01-21 22:57:06 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='MyDiscord',
     author='justinoboyle',
     url='https://github.com/justinoboyle/MyDiscord',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='Adds custom CSS and JavaScript support to Discord. (Fork of BeautifulDiscord)',
     long_description=readme,
