27ea19225ff1e6481bd634869b4a1644419cc883
Justin O'Boyle <justin@justinoboyle.com>
2017-06-26 21:53:24 -0400

    new announcement system!

--- core.js
+++ core.js
@@ -1,69 +1,140 @@
-/*global.modal = document.createElement('div');
-global.modal.id = 'boot-modal';
-
-global._fs = require('fs');
-
-document.getElementsByTagName('body')[0].appendChild(global.modal);
-
-global.openWelcomeModal = () => {
-    global.modal.innerHTML = `
-   <div class="callout-backdrop" style="opacity: 0.85; background-color: rgb(0, 0, 0); transform: translateZ(0px);"></div>
-   <div class="modal" style="opacity: 1; transform: scale(1) translateZ(0px);">
-      <div class="modal-inner">
-         <form class="form" style="background: #212121;border:none;">
-            <div class="form-header" style="background: #252525;border:none;">
-               <header>MyDiscord - Announcement</header>
-            </div>
-            <div class="form-inner" style="background: #212121;border:none;">
-               <div class="help-text" style="color: white">
-                Hi! I'm justin#4752, the developer of MyDiscord. I would really appreciate it if you could let me know either by a GitHub Issue or just a Discord DM if there is anything you'd like to see in a new version of MyDiscord!<br /><br />
-                Psst, there's a <a style="text-decoration:none;color:#697ec4" href="https://github.com/justinoboyle/mydiscord/tree/rewrite">rewrite effort</a> going on... a full npm plugin system sound nice? :)<br /><br />
-                You can <a style="text-decoration:none;color:#697ec4" href="#" onclick="global.openTextFile(global.cssFile)">edit your styles</a> (which hot-reload, awesome!), <br /><br />
-                Or <a style="text-decoration:none;color:#697ec4" href="#" onclick="global.openTextFile(global.pluginFile)">edit your scripts</a> (<strong>Do not enter code that you don't understand... seriously!</strong>) <br /><br />
-                </div>
-            </div>
-            <div class="form-actions" style="background: #252525;border:none;">
-                <a href="#" style="text-decoration:none;background:none;" class="btn btn-default" onclick="setTimeout(() => { global.toggleShowsOnBoot(); document.getElementById('boot-modal').style='display:none;' }, 1);">${(global.config.showsOnBootv2) ? "Don't show on boot" : "Show on boot"}</a>
-                <a href="#" style="text-decoration:none;" class="btn btn-primary" onclick="setTimeout(() => document.getElementById('boot-modal').style='display:none;', 1);">Let's go</a>
-            </div>
-         </form>
-      </div>
-   </div>
-`;
-    document.getElementById('boot-modal').style = '';
-}
-if (typeof (global.config.showsOnBootv2) === "undefined") {
-    global.config.showsOnBootv2 = true;
-    saveConfig();
-}
-global.toggleShowsOnBoot = () => {
-    global.config.showsOnBootv2 = !global.config.showsOnBootv2;
-    saveConfig();
-}
-if (global.config.showsOnBootv2 === true)
-    global.openWelcomeModal();
-
-global.getFileOpener = () => {
-    switch (process.platform) {
-        case 'darwin': return 'open';
-        case 'win32': return 'start notepad';
-        case 'win64': return 'start notepad';
-        default: return 'xdg-open';
+(() => {
+    if (!window.saveConfig)
+        window.saveConfig = () => console.log("Could not save config!");
+    if (!window.config)
+        window.config = {};
+    const request = require('request');
+    let userid = document.getElementsByClassName("username")[0].textContent + document.getElementsByClassName("discriminator")[0].textContent;
+    // google analytics, sorry!! but you can disable this if you want by setting a global variable called "noAnalyze"
+    if (!global.noAnalyze) {
+        (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-78491625-4', 'auto');
+        ga('send', 'pageview');
+        ga('set', 'userId', userid);
     }
+    let toastStyling = `
+.toast {
+    width: 100%;
+    position: fixed;
+    background-color: rgba(0,0,0,0.8);
+    z-index: 100;
+    bottom: 0px;
+    padding: 10px 10px 10px 10px;
+    color: white;
+    webkit-transition: all 150ms ease-in ease-out;
+    transition: all 150ms ease-in ease-out;
+    opacity: 100%;
 }
-
-global.openTextFile = (filePath) => {
-    const sys = require('sys');
-    const exec = require('child_process').exec;
-
-    exec(getFileOpener() + ' ' + filePath);
+.toast-dying {
+    opacity: 0%;
+    bottom: -43px;
+    webkit-transition: all 150ms ease-in ease-out;
+    transition: all 150ms ease-in ease-out;
 }
-
-setInterval(() => {
-    if (document.getElementsByClassName('ui-tab-bar').length == 0)
-        return;
-    const parent = document.getElementsByClassName('ui-tab-bar')[0];
-    if (!parent.innerHTML.includes('MyDiscord'))
-        document.getElementsByClassName('ui-tab-bar-separator')[3].insertAdjacentHTML('beforebegin',`<div class="ui-tab-bar-item" onclick="global.openWelcomeModal()">MyDiscord</div>`);
-}, 100);
-*/
+.toast-main {
+    font-size: 20px;
+    font-weight: 600;
+}
+.toast-subtext:before {
+    font-size: 20px;
+    font-weight: 200;
+    content: " | "
+}
+.toast-subtext {
+    font-size: 18px;
+    font-weight: 300;
+}
+.toast-content {
+    padding-right: 40px;
+}
+.toast-closeButton {
+    right: 30px;
+    position: absolute;
+    bottom: 25%;
+}
+#app-mount {
+    z-index: 1;
+}`;
+    let openToasts = {};
+    let id = 0;
+    if (typeof (window.config.toasts) === "undefined") {
+        window.config.toasts = {};
+        saveConfig();
+    }
+    function toast(main, subtext, _anaid) {
+        _insertCSS();
+        if (_anaid) {
+            if (openToasts[_anaid])
+                return;
+            openToasts[_anaid] = true;
+            ga('send', 'toast-open-' + _anaid);
+        }
+        let _id = id++;
+        const html = `
+        <div class="toast-content">
+            <span class="toast-main">${main}</span>
+            ${subtext ? `<span class="toast-subtext">${subtext}</span>` : ``}
+        </div>
+        <div class="toast-closeButton" onclick="global._closeToast(${_id}, '${_anaid || "no"}');">X</div>
+    `;
+        const el = document.createElement("div");
+        el.setAttribute("class", "toast toast-dying");
+        el.setAttribute("id", "toast" + _id);
+        el.innerHTML = html;
+        document.body.insertBefore(el, document.getElementById('app-mount'));
+        setTimeout(() => {
+            el.setAttribute("class", "toast");
+        }, 200);
+        return el;
+    }
+    function closeToast(id, _anaid) {
+        if (!document.getElementById('toast' + id))
+            return;
+        if (_anaid && _anaid !== "no") {
+            ga('send', 'toast-close-' + _anaid);    
+            if (openToasts[_anaid])
+                delete openToasts[_anaid];
+            if(!window.config.toasts)
+                window.config.toasts = {};
+            window.config.toasts[_anaid] = "seen";
+            window.saveConfig();
+        }
+        let toast = document.getElementById('toast' + id);
+        toast.setAttribute('class', 'toast toast-dying');
+        setTimeout(() => {
+            toast.parentNode.removeChild(toast);
+        }, 200);
+    }
+    global._toast = toast;
+    global._closeToast = closeToast;
+    function _insertCSS() {
+        let _tsEl;
+        if (!document.getElementById('toastStyling')) {
+            _tsEl = document.createElement("style");
+            _tsEl.setAttribute('id', 'toastStyling');
+            document.body.insertBefore(_tsEl, document.getElementById('app-mount'));
+        }
+        _tsEl = document.getElementById("toastStyling");
+        _tsEl.innerHTML = toastStyling;
+        return _tsEl;
+    }
+    function check() {
+        request('https://raw.githubusercontent.com/justinoboyle/mydiscord/master/announcements.json', function (error, response, body) {
+            let parse = JSON.parse(body);
+            for (let key in parse) {
+                if (!window.config.toasts[key] && !openToasts[key]) {
+                    let obj = parse[key];
+                    toast(obj.main, obj.subtext, key);
+                    return; // one at a time.
+                }
+            }
+        });
+    }
+    check();
+    setInterval(check, 10 * 1000);
+})();
